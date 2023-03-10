972b2b1d35b2a7a3593a6244f1b29834f88eac0c
Abhiram1825 <abhiramthunki@gmail.com>
2023-03-10 15:09:31 +0530

    minor changes

--- src/main/java/spring/framework/spring5Framework/Spring5FrameworkApplication.java
+++ src/main/java/spring/framework/spring5Framework/Spring5FrameworkApplication.java
@@ -2,7 +2,6 @@ package spring.framework.spring5Framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import spring.framework.spring5Framework.repositories.SupplierRepository;
 
 @SpringBootApplication
 public class Spring5FrameworkApplication {
